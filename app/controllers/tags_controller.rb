2d0c3ca1831fee4040e413c3f5177c261e93bb59
Pradeep <you@example.com>
2016-04-28 15:27:19 +0530

    fix tags on signup

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   before_action :set_tag, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   # GET /tags
   # GET /tags.json
